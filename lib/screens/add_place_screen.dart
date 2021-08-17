cff9c7d9be0de9a6e727e4ff666668d75cdb5cbb
mahdiyarz <m.arbabzi@gmail.com>
2021-08-17 15:22:58 +0430

    Connect routeName of add_place_screen on main.dart

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
