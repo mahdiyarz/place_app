3ca9b2f4f510dbea262d09b408e3ae0dfae3752c
mahdiyarz <m.arbabzi@gmail.com>
2021-08-17 13:46:33 +0430

    Set first look on place_list_screen.dart

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
