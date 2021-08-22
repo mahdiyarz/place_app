e6a387de129106aefa0996b7fbdd0a37edbd607f
mahdiyarz <m.arbabzi@gmail.com>
2021-08-22 14:53:05 +0430

    Config first look of location_input_widget

--- lib/widgets/location_input_widget.dart
+++ lib/widgets/location_input_widget.dart
@@ -8,10 +8,38 @@ class LocationInputWidget extends StatefulWidget {
 }
 
 class _LocationInputWidgetState extends State<LocationInputWidget> {
+  String? _previewImageUrl;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.black54)),
+          width: double.infinity,
+          height: 150,
+          margin: EdgeInsets.only(top: 10),
+          alignment: Alignment.center,
+          child: _previewImageUrl == null
+              ? Text('Location not chosen yet')
+              : Image.network(_previewImageUrl as String),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.location_on),
+              label: Text('Current Location'),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.map),
+              label: Text('Choose from Map'),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
